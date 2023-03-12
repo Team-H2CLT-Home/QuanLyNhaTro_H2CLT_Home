44913454924681856efaebb8f61aff1ecd8e01aa
long2 <longmhph29041@fpt.edu.vn>
2023-03-12 18:00:50 +0700

    ManHinhDangNhap_Ph29041_1203

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_home/MainActivity.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_home/MainActivity.kt
@@ -2,11 +2,14 @@ package h2clt.fpt.quanlynhatro_h2clt_home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import h2clt.fpt.quanlynhatro_h2clt_home.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Hello Everyone
         //hiep đã ở đây
 
