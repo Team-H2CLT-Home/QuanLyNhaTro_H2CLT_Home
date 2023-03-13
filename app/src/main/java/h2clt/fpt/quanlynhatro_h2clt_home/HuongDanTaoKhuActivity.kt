6312c3bb6a72e80701ad8d1f491d432240adbb2e
LE TIEN THANH <thanhltph29166@fpt.edu.vn>
2023-03-13 12:15:48 +0700

    main_ph29166

---
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_home/HuongDanTaoKhuActivity.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.quanlynhatro_h2clt_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HuongDanTaoKhuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huong_dan_tao_khu)
+    }
+}
