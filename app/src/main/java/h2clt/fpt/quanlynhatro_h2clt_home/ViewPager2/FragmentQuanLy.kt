38216856aa2675779601dd592f861702fc76431c
long2 <longmhph29041@fpt.edu.vn>
2023-03-14 12:46:18 +0700

    tablayout fragment man hinh chinh ph29041

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_home/ViewPager2/FragmentQuanLy.kt
+++
@@ -1,7 +0,0 @@
-package h2clt.fpt.quanlynhatro_h2clt_home.ViewPager2
-
-import androidx.fragment.app.Fragment
-import h2clt.fpt.quanlynhatro_h2clt_home.R
-
-class FragmentQuanLy: Fragment(R.layout.tablayout_quanly) {
-}
