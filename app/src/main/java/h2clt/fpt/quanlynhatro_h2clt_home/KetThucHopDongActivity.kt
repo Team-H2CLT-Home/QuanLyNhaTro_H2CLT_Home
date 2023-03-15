dee7d116568568e733736bb2112a105ed1b0b5fc
LE TIEN THANH <thanhltph29166@fpt.edu.vn>
2023-03-15 23:12:12 +0700

    xu_ly_phong_ph29166_15/03

---
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_home/KetThucHopDongActivity.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.quanlynhatro_h2clt_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KetThucHopDongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ket_thuc_hop_dong)
+    }
+}
